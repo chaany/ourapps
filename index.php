61d0e27aff3f54e4140c5e5a8ce55f58152ca69e
chaany <99613483+chaany@users.noreply.github.com>
2022-06-03 00:07:18 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,131 @@
+<!-- <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coder Zizo </title>
+    <link rel="icon" href="app.png">
+    
+
+</head>
+<body dir="rtl">
+
+<form>
+
+</form>
+
+<?php 
+
+?>
+
+</body>
+</html> -->
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> coders @zizo && @mero apps </title>
+    <link rel="icon" href="logo.jpg">
+    
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</head>
+<body style="background-color:#cce5ff;">
+
+    <h1 style="text-align: center; background-color: blueviolet; "> Coders @zizo && @mero apps</h1>
+
+
+
+</div>
+<div>
+        <div class="card" style="width: 18rem; text-align: center; margin: auto; margin-top: 50px; margin-bottom: 0px;">
+            
+            <div class="card-body">
+              <h5 class="card-title">AUDIO PLAYER مشغل صوت by zizo</h5>
+              <a target="_blank" href="https://chaany.github.io/app8/audio_player.html" class="btn btn-primary">انقر للذهاب للموقع</a>
+            </div>
+          </div>
+    <div>
+        <div class="card" style="width: 18rem; text-align: center; margin: auto; margin-top: 50px; margin-bottom: 0px;" >
+            
+            <div class="card-body">
+              <h5 class="card-title">About Coder Zizo by zizo</h5>
+              <a target="_blank" href="https://chaany.github.io/aboutzizo/index.html" class="btn btn-primary">انقر للذهاب للموقع</a>
+            </div>
+          </div>
+    </div>
+    
+    <div>
+        <div class="card" style="width: 18rem; text-align: center; margin: auto; margin-top: 50px; margin-bottom: 0px;">
+            
+            <div class="card-body">
+              <h5 class="card-title">Abot Coder mero by mero</h5>
+              <a target="_blank" href="https://chaany.github.io/aboutmero/main.html" class="btn btn-primary">انقر للذهاب للموقع</a>
+            </div>
+          </div>
+    </div>
+
+    <div>
+        <div class="card" style="width: 18rem; text-align: center; margin: auto; margin-top: 50px; margin-bottom: 0px;">
+            
+            <div class="card-body">
+              <h5 class="card-title">kitchen design by zizo</h5>
+              <a target="_blank" href="https://chaany.github.io/kitchen/index.html" class="btn btn-primary">انقر للذهاب للموقع</a>
+            </div>
+          </div>
+    </div>
+
+    <div>
+        <div class="card" style="width: 18rem; text-align: center; margin: auto; margin-top: 50px; margin-bottom: 0px;">
+            
+            <div class="card-body">
+              <h5 class="card-title">تشفير و فك تشفير النص by zizo</h5>
+              <a target="_blank" href="https://chaany.github.io/decoder/index.html" class="btn btn-primary">انقر للذهاب للموقع</a>
+            </div>
+          </div>
+    </div>
+    
+    <div>
+        <div class="card" style="width: 18rem; text-align: center; margin: auto; margin-top: 50px; margin-bottom: 0px;">
+            
+            <div class="card-body">
+              <h5 class="card-title">محرر اكواد by zizo</h5>
+              <a target="_blank" href="https://zizowebsnewmadedapps2022.on.drv.tw/editorzizo/app.html" class="btn btn-primary">انقر للذهاب للموقع</a>
+            </div>
+          </div>
+    </div>
+
+    <div>
+        <div class="card" style="width: 18rem; text-align: center; margin: auto; margin-top: 50px; margin-bottom: 0px;">
+            
+            <div class="card-body">
+              <h5 class="card-title">Bwitter by mero</h5>
+              <a target="_blank" href="https://chaany.github.io/Bwitter/main.html" class="btn btn-primary">انقر للذهاب للموقع</a>
+            </div>
+          </div>
+    </div>
+
+    <div>
+        <div class="card" style="width: 18rem; text-align: center; margin: auto; margin-top: 50px; margin-bottom: 0px;">
+            
+            <div class="card-body">
+              <h5 class="card-title">Bwitter android by mero</h5>
+              <a target="_blank" href="https://chaany.github.io/androidBwitter/main.html" class="btn btn-primary">انقر للذهاب للموقع</a>
+            </div>
+          </div>
+      
+          <div class="alert alert-primary" role="alert">
+             <pre  style="text-align: center; font-size: 4.5vw;"> About US
+    " we are web developers team "   
+<mark style="font-size: xx-large;">by coders zizo and mero</mark>
+             </pre>
+          </div>
+        
+    <!-- text-align: center; margin: auto; margin-top: 50px; margin-bottom: 0px; -->
+</body>
+</html>
+
